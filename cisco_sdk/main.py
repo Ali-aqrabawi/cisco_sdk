1701ec9d74f92699c69a5fc75316b9fdde590b40
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2018-08-22 19:49:54 +0300

    remove main.py

--- cisco_sdk/main.py
+++
@@ -1,9 +0,0 @@
-from cisco_sdk.devices.switches import CatSwitch
-
-my_swicth = CatSwitch(host='4.71.144.98', username='admin', password='admin')
-
-my_swicth.sync_interfaces()
-
-
-for i in my_swicth.interfaces:
-    print(i.name , " is " , i.link_status)
