b022eeec4f1d1df858bedd31a9d5213069933e1c
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2018-08-28 00:32:41 +0300

    remove easysnmp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cisco_sdk",
-    version="0.0.8",
+    version="0.0.9",
     author="ali aqrabawi",
     author_email="ali_aqrabawi@yahoo.com",
     description="cisco devices SDK",
