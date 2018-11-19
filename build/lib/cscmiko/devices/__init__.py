e923c5bf940734ad3b034820de34d6bde0d2f837
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2018-11-19 23:06:44 +0200

    remove dist+build

--- build/lib/cscmiko/devices/__init__.py
+++
@@ -1,4 +0,0 @@
-from cscmiko.devices.switches.catalyst.generic import CatSwitch
-from cscmiko.devices.switches.nexus.generic import NexusSwitch
-
-__all__ = ['CatSwitch', 'NexusSwitch']
--- build/lib/cscmiko/models/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .layer2_utils import translate_interface_name, clean_link_status, cidr_to_netmask, exteract_software_version, \
-    extract_device_name,is_switchport
-
-from.layer3_utils import ROUTE_PROTOCOL
